03d94ff5d999cd361a29575a29df9615be1aa1f4
sod-d <soduduracle@gmail.com>
2025-03-06 23:45:21 +0900

    Initial Commit

---
+++ HamsterSalad/HamsterSaladApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterSaladApp.swift
+//  HamsterSalad
+//
+//  Created by soyoung kim on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HamsterSaladApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HamsterSaladTests/HamsterSaladTests.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterSaladTests.swift
+//  HamsterSaladTests
+//
+//  Created by soyoung kim on 3/6/25.
+//
+
+import Testing
+@testable import HamsterSalad
+
+struct HamsterSaladTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
