5d1d03d9e74d4c01af6dbe649118fd9abe07938d
sod-d <soduduracle@gmail.com>
2025-03-07 00:02:53 +0900

    project init - TabView add - card style UI add

--- HamsterSalad/ContentView.swift
+++ HamsterSalad/ContentView.swift
@@ -9,12 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }
+            
+            recomendView()
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("추천")
+                }
+            
+            NewspaperView()
+                .tabItem {
+                    Image(systemName: "newspaper.fill")
+                    Text("새소식")
+                }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "face.smiling.inverse")
+                    Text("MY")
+                }
         }
+    }
+}
+
+// 각 화면의 기본 구조
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            CardView() // 카드 UI 표시
+            Spacer()
+        }
+    }
+}
+
+struct recomendView: View {
+    var body: some View {
+        Text("추천 화면")
+    }
+}
+
+struct NewspaperView: View {
+    var body: some View {
+        Text("새소식 화면")
+    }
+}
+struct ProfileView: View {
+    var body: some View {
+        Text("MY 화면")
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("소영님의 순자산")
+                .font(.headline)
+                .foregroundColor(.white)
+            Text("₩123,456,789")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.green)
+        .cornerRadius(15)
+        .shadow(radius: 5)
         .padding()
     }
 }
