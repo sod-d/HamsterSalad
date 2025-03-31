de18255348603e1317c0ec01c35f21a44f64c95c
sod-d <soduduracle@gmail.com>
2025-03-31 22:16:24 +0900

    cardView, accountsListView add

---
+++ HamsterSalad/AccountViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountViewModel.swift
+//  HamsterSalad
+//
+//  Created by soyoung kim on 3/7/25.
+//
+
+import Foundation
---
+++ HamsterSalad/TransactionHistroy.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionHistroy.swift
+//  HamsterSalad
+//
+//  Created by soyoung kim on 3/7/25.
+//
+
+import SwiftUI
+
+struct TransactionHistroy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionHistroy()
+}
