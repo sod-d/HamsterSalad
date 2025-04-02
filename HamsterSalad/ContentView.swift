ead21b7a9d734c5b668f9022ea6e4d68176b5d10
sodud <sodud@uracle.co.kr>
2025-04-02 16:51:47 +0900

    MainIconMenuVIew add

--- HamsterSalad/ContentView.swift
+++ HamsterSalad/ContentView.swift
@@ -41,7 +41,8 @@ struct HomeView: View {
     var body: some View {
         VStack {
             CardView() // 카드 UI 표시
-            Spacer()
+//            Spacer()
+            MainIconMenuView()
         }
     }
 }
@@ -80,6 +81,38 @@ struct CardView: View {
         .cornerRadius(15)
         .shadow(radius: 5)
         .padding()
+    }
+}
+
+//MainIconList
+struct MainIconMenuView: View {
+    let menuItems = [
+        ("chart.bar.fill", "자산"),
+        ("dollarsign.circle.fill", "혜택"),
+        ("doc.text.fill", "소비"),
+        ("person.crop.circle.fill", "MY")
+    ]
+    var body: some View{
+        HStack {
+            ForEach(menuItems, id: \.0) { item in
+                Spacer() //균등하게 배치를 위해 Spacer 추가
+                VStack {
+                    Image(systemName: item.0)
+                        .font(.system(size:40))
+                        .foregroundStyle(.gray)
+                    Text(item.1)
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                }
+                Spacer() //균등하게 배치를 위해 Spacer 추가
+
+            }
+        }
+        .padding(.vertical, 15) // 위아래 여백 추가
+        .frame(maxWidth: .infinity) // 가로 최대 크기 확장
+        .background(Color.white) // 배경색 추가 (필요하면)
+//        .cornerRadius(10) // 모서리 둥글게
+//        .shadow(radius: 3) // 그림자 추가 (선택사항)
     }
 }
 
